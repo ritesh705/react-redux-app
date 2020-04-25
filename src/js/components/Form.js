b8b7567be4c7bc9de838ba890d253e6c713c1486
Ritesh Kumar <ritesh705@gmail.com>
2020-04-25 23:58:34 +0530

    update

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -23,23 +23,18 @@ class ConnectedForm extends Component
   handleSubmit(event)
   {
     event.preventDefault();
-    const { title } = this.state;
-    this.props.addArticle({ title });
-    this.setState({ title: "" });
+    const title = this.state;
+    this.props.addArticle(title);
+    this.setState({title: ""});
   }
 
   render() {
-    const { title } = this.state;
+    const title = this.state.title;
     return (
       <form onSubmit={this.handleSubmit}>
         <div>
           <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            value={title}
-            onChange={this.handleChange}
-          />
+          <input type="text" id="title" value={title} onChange={this.handleChange} />
         </div>
         <button type="submit">SAVE</button>
       </form>
